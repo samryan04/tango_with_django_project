bac31ce8f43aaf6280216dfcee5f5a2f017744a0
Sam Ryan <2787121r@student.gla.ac.uk>
2024-01-28 17:44:23 +0000

    chapter 4 complete

--- rango/views.py
+++ rango/views.py
@@ -8,4 +8,4 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br> <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
